541e08d4d376cc5ad3819f9ca6b6a1e84fa7a507
Adrian Abi Saleh <aabisaleh@insa-lyon.fr>
2024-05-25 13:31:22 +0200

    fin de la fonctionnalite demandeConsultation, juste petit bug avec la message box

--- src/main/java/Controleur/DemandeConsultationAction.java
+++ src/main/java/Controleur/DemandeConsultationAction.java
@@ -6,6 +6,7 @@
 package Controleur;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import metier.modele.Client;
 import metier.modele.Consultation;
 import metier.modele.Medium;
@@ -19,10 +20,12 @@ public class DemandeConsultationAction extends Action {
 
     @Override
     public void execute(HttpServletRequest req) {
+        HttpSession session = req.getSession(true);
+        System.out.println("Demande de consultation action pour :" + (Long) session.getAttribute("ID") + " " + Long.parseLong(req.getParameter("idMedium")));
         Service service = new Service();
         
-        Client c = service.rechercherClientParID((Long) req.getAttribute("ID")); // retrouver l'objet client grace a l'identifiant ajouter a la session
-        Medium m = service.rechercherMediumParID((Long) req.getAttribute("idMedium")); // recuperer le medium demander qui sera ajouter dans la request lorsque l'utilisateur clique dessus dans la page demander un medium
+        Client c = service.rechercherClientParID((Long) session.getAttribute("ID")); // retrouver l'objet client grace a l'identifiant ajouter a la session
+        Medium m = service.rechercherMediumParID(Long.parseLong(req.getParameter("idMedium"))); // recuperer le medium demander qui sera ajouter dans la request lorsque l'utilisateur clique dessus dans la page demander un medium
         
         Consultation consultation = service.demanderConsultation(c, m); // demande de consultation lancee dans le back
         if(consultation == null) // donc la demande de consultation n'a pas aboutie
