b06ad3df055cca7c8f0234e0545e8ef1dd9371a7
Adrian Abi Saleh <aabisaleh@insa-lyon.fr>
2024-05-27 15:44:33 +0200

    rien

--- src/main/java/Controleur/ConsultationInitAction.java
+++ src/main/java/Controleur/ConsultationInitAction.java
@@ -19,18 +19,11 @@ public class ConsultationInitAction extends Action {
     @Override
     public void execute(HttpServletRequest req){
         Service service = new Service();
-<<<<<<< HEAD
+
         Long idConsultation = Long.parseLong(req.getParameter("idCons"));
         System.out.println("dans la consultation init action, id:" + idConsultation);
-
-        Consultation cons = service.rechercherConsultationById(idConsultation);
-=======
-        Long idConsultation = (Long) req.getAttribute("idConsultation");
-        Consultation cons = service.rechercherConsultationParID(idConsultation);
->>>>>>> 5b2200f323e7017e5a6c2d4d3006220d5b46ad83
-        
-        Client c = cons.getClient();
-        
+        Consultation cons = service.rechercherConsultationById(idConsultation);    
+        Client c = cons.getClient(); 
         req.setAttribute("client", c);
         req.setAttribute("prenomMedium", cons.getMedium().getDenomination());
     }
